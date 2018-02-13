b116942d9feaaf222c782b5246b7bde3ed693266
Seb Ashton <seb@sebashton.com>
2018-02-13 15:02:04 +0000

    _shrug_

--- app/models/spree/assemblies_part.rb
+++ app/models/spree/assemblies_part.rb
@@ -3,7 +3,7 @@ module Spree
     belongs_to :assembly, :class_name => "Spree::Product",
       :foreign_key => "assembly_id", touch: true
 
-    belongs_to :part, :class_name => "Spree::Variant", :foreign_key => "part_id"
+    belongs_to :part, :class_name => "Spree::Product", :foreign_key => "part_id"
 
     def self.get(assembly_id, part_id)
       find_or_initialize_by(assembly_id: assembly_id, part_id: part_id)
