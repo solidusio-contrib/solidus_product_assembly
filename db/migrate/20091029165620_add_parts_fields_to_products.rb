169bcc8ff2d3d5ec90de202842b34fd2f7831d9f
Angel Perez <iAngel.p93@gmail.com>
2018-12-24 10:43:10 +0100

    Replace table_exists? -> data_source_exists?

--- db/migrate/20091029165620_add_parts_fields_to_products.rb
+++ db/migrate/20091029165620_add_parts_fields_to_products.rb
@@ -1,24 +1,24 @@
 class AddPartsFieldsToProducts < SolidusSupport::Migration[4.2]
   def self.up
-    table = if table_exists?(:products)
+    table = if data_source_exists?(:products)
       'products'
-    elsif table_exists?(:spree_products)
+    elsif data_source_exists?(:spree_products)
       'spree_products'
-    end 
-    
+    end
+
     change_table(table) do |t|
       t.column :can_be_part, :boolean, :default => false, :null => false
       t.column :individual_sale, :boolean, :default => true, :null => false
-    end  
+    end
   end
 
   def self.down
-    table = if table_exists?(:products)
+    table = if data_source_exists?(:products)
       'products'
-    elsif table_exists?(:spree_products)
+    elsif data_source_exists?(:spree_products)
       'spree_products'
-    end 
-    
+    end
+
     change_table(table) do |t|
       t.remove :can_be_part
       t.remove :individual_sale
