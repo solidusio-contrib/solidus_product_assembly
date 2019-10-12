46119c1d484c72dd1f7f3f170658efd587ff6556
Thomas Støa Pettersen <thomas@forgetit.no>
2019-10-12 14:13:31 +0200

    Zeitwerk compatibility fix

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,6 +1,6 @@
-module Spree
+module Spree::CheckoutControllerDecorator
   if defined? Spree::Frontend
-    CheckoutController.class_eval do
+    Spree::CheckoutController.class_eval do
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
       def before_payment; end
