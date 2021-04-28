adc350bf166cd77b18651ff5eabd7ba8d8a68900
Alessandro Desantis <desa.alessandro@gmail.com>
2021-04-28 10:44:33 +0200

    Remove frontend overrides/decorators and streamline the rest

--- app/overrides/add_admin_product_form_fields.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/products/_form",
-  name: "product_assembly_admin_product_form_right",
-  insert_bottom: "[data-hook='admin_product_form_right']",
-  partial: "spree/admin/products/product_assembly_fields",
-  disabled: false,
-)
