675b33f016638bbb717bc01aeb984248534e6a91
Alistair Norman <alistair@super.gd>
2022-06-08 10:14:58 -0700

    Add a spec for search_can_be_part
    I want to update this method to be able to handle translations so I want
    to document the original behaviour so that it is not compromised in the
    update.

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -8,6 +8,24 @@ describe Spree::Product do
     @master_variant = Spree::Variant.where(is_master: true).find_by(product_id: @product.id)
   end
 
+  describe ".search_can_be_part" do
+    subject { described_class.search_can_be_part("matching") }
+
+    let!(:name_matching_product) { create :product, can_be_part: true, name: "matching" }
+    let!(:sku_matching_product) { create :product, can_be_part: true, master: variant }
+    let(:variant) { create :master_variant, sku: "matching" }
+
+    before do
+      create :product, can_be_part: false, name: "matching"
+      create :product, deleted_at: 1.day.ago, can_be_part: true, name: "matching"
+      create :product, can_be_part: true, name: "Something else"
+    end
+
+    it "returns non-deleted products matching the search that can be parts" do
+      expect(subject).to contain_exactly(name_matching_product, sku_matching_product)
+    end
+  end
+
   describe "Spree::Product Assembly" do
     before do
       @product = create(:product)
