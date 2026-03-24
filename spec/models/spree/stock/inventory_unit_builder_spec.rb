5f2a379259a9e0d86d6cf6c8d09dbd9434a9a616
Alistair Norman <alistair@super.gd>
2026-03-24 09:24:26 -0700

    Update inventory unit builder spec
    I don't know why this test had the wrong count. There are only three
    parts in the shared context. Testing literals here makes things a little
    simpler.

--- spec/models/spree/stock/inventory_unit_builder_spec.rb
+++ spec/models/spree/stock/inventory_unit_builder_spec.rb
@@ -9,16 +9,13 @@ module Spree
 
       context "order shares variant as individual and within bundle" do
         include_context "product is ordered as individual and within a bundle" do
-          let(:bundle_item_quantity) { order.find_line_item_by_variant(bundle_variant).quantity }
-
           describe "#units" do
             it "returns an inventory unit for each part of each quantity for the order's line items" do
               units = subject.units
-              expect(units.count).to eq 4
-              expect(units[0].line_item.quantity).to eq order.line_items.first.quantity
-              expect(units[0].line_item.quantity).to eq bundle_item_quantity
+              expect(units.count).to eq 3
+              expect(units[0].line_item.quantity).to eq 1
 
-              line_item = order.line_items.first
+              line_item = order.line_items.sole
 
               expect(units.map(&:variant)).to match_array line_item.parts
             end
