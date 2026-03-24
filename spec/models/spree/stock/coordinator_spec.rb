80e0e87d3a92d68ab47e07c23c0a401a74f13652
Alistair Norman <alistair@super.gd>
2026-03-23 17:09:15 -0700

    Improve coordinator spec
    This is at least somewhat more accurately testing what I think the
    purpose of this test is by checking that the inventory units match the
    parts instead of doing complicated math and checking that there are
    maybe the right number of them.

--- spec/models/spree/stock/coordinator_spec.rb
+++ spec/models/spree/stock/coordinator_spec.rb
@@ -2,58 +2,37 @@
 
 require 'spec_helper'
 
-# Spree::Stock::Coordinator was refactored in Solidus to hide keep private
-# information about packages, we have to be sneaky to reach inside
-def inventory_units(coordinator)
-  coordinator.shipments.flat_map(&:inventory_units)
-end
+RSpec.describe Spree::Stock::SimpleCoordinator do
+  describe '#shipments' do
+    subject { described_class.new(order) }
 
-module Spree
-  module Stock
-    coordinator_class =
-      if Spree.solidus_gem_version < Gem::Version.new('2.4.x')
-        Coordinator
-      else
-        SimpleCoordinator
-      end
-    describe coordinator_class do
-      subject { described_class.new(order) }
+    context "order shares variant as individual and within bundle" do
+      include_context "product is ordered as individual and within a bundle"
 
-      context "order shares variant as individual and within bundle" do
-        include_context "product is ordered as individual and within a bundle"
+      before { Spree::StockItem.update_all(count_on_hand: 10) }
 
-        before { StockItem.update_all 'count_on_hand = 10' }
+      context "bundle part requires more units than individual product" do
+        before { order.contents.add(bundle_variant, 5) }
 
-        context "bundle part requires more units than individual product" do
-          before { order.contents.add(bundle_variant, 5) }
-
-          let(:bundle_item_quantity) { order.find_line_item_by_variant(bundle_variant).quantity }
-
-          it "calculates items quantity properly" do
-            expected_units_on_package = order.line_items.to_a.sum(&:quantity) - bundle_item_quantity + (bundle.parts.count * bundle_item_quantity)
-
-            expect(inventory_units(subject).size).to eql expected_units_on_package
-          end
+        it "includes inventory units for all bundle parts" do
+          variant_ids = subject.shipments.flat_map(&:inventory_units).map(&:variant_id)
+          expect(variant_ids).to contain_exactly(*bundle.parts.map(&:id))
         end
       end
+    end
 
-      context "multiple stock locations" do
-        let!(:stock_locations) { (1..3).map { create(:stock_location) } }
+    context "multiple stock locations" do
+      let!(:stock_locations) { (1..3).map { create(:stock_location) } }
+      let(:order) { create(:order_with_line_items) }
+      let(:parts) { (1..3).map { create(:variant) } }
+      let(:bundle_variant) { order.variants.first }
+      let(:bundle) { bundle_variant.product }
 
-        let(:order) { create(:order_with_line_items) }
-        let(:parts) { (1..3).map { create(:variant) } }
+      before { bundle.parts << parts }
 
-        let(:bundle_variant) { order.variants.first }
-        let(:bundle) { bundle_variant.product }
-
-        let(:bundle_item_quantity) { order.find_line_item_by_variant(bundle_variant).quantity }
-
-        before { bundle.parts << parts }
-
-        it "haha" do
-          expected_units_on_package = order.line_items.to_a.sum(&:quantity) - bundle_item_quantity + (bundle.parts.count * bundle_item_quantity)
-          expect(inventory_units(subject).size).to eql expected_units_on_package
-        end
+      it "includes inventory units for all bundle parts" do
+        variant_ids = subject.shipments.flat_map(&:inventory_units).map(&:variant_id)
+        expect(variant_ids).to contain_exactly(*bundle.parts.map(&:id))
       end
     end
   end
