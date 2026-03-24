d06fd0bf6e220f41c2c2fe9a38b6d029930f1281
Alistair Norman <alistair@super.gd>
2026-03-23 21:37:44 -0700

    Fix bundle shared context
    I don't know why this ever worked. Maybe the order with line items
    factory used to use a particular product but as it exists now, this was
    not using the bundle product for the order.

--- spec/support/shared_contexts/order_with_bundle.rb
+++ spec/support/shared_contexts/order_with_bundle.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 shared_context "product is ordered as individual and within a bundle" do
-  let(:order) { create(:order_with_line_items) }
-  let(:parts) { (1..3).map { create(:variant) } }
-
-  let(:bundle_variant) { order.variants.first }
-  let(:bundle) { bundle_variant.product }
-
+  let!(:order) { create(:order_with_line_items, line_items_attributes: [{ product: bundle }]) }
+  let(:parts) { create_list(:variant, 3) }
+  let(:bundle) { create :product, parts: }
+  let(:bundle_variant) { bundle.master }
   let(:common_product) { order.variants.last }
-
-  before do
-    bundle.parts << [parts, common_product]
-  end
 end
